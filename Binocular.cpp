c26cc0cebc03d45fbb3723deb376de9172a870cf
Evergardener <henry0927@outlook.com>
2022-05-02 22:13:25 +0800

    Update Binocular.cpp

--- Binocular.cpp
+++ Binocular.cpp
@@ -4,13 +4,55 @@
 #include <opencv2/opencv.hpp>
 #include <cmath>
 #define N 1
-#define FOC_IN_PIX 928.334  // iPhone 13 Pro广角摄像头像素级焦距
+#define FOC_IN_PIX 928.334  // focal in px (use your own, can be retrieved by using the)
 
 using namespace std;
 using namespace cv;
 
 Rect* l_ROI = new Rect[N];
 Rect* r_ROI = new Rect[N];
+
+void proc(Mat& binary, Rect* rect, int size);
+void getColorMask(Mat& src, Mat& dst, Mat& mask);
+void getCenter(Point2f& center, Rect* rect, int size);
+double calcBinocularDist(Point2f left_pos1, Point2f right_pos1, Point2f left_pos2, Point2f right_pos2, double f, double T);
+
+int main() {
+    Point2f l_centerImg_fir, l_centerImg_sec;
+    Point2f r_centerImg_fir, r_centerImg_sec;
+    double wldDist = 0;     // where the displacement result is stored, in mms (millimeters)
+    Mat l_src_fir, l_dst_fir;
+    Mat l_src_sec, l_dst_sec;
+    Mat r_src_fir, r_dst_fir;
+    Mat r_src_sec, r_dst_sec;
+    Mat l_mask, r_mask;
+    double T = 50, f = FOC_IN_PIX;
+    double standard = 55.0;
+
+    l_src_fir = imread("");     // file path of the left capture in the first binocular pos 
+    l_src_sec = imread("");     // file path of the left capture in the second binocular pos 
+    r_src_fir = imread("");     // file path of the right capture in the first binocular pos 
+    r_src_sec = imread("");     // file path of the right capture in the second binocular pos 
+
+    getColorMask(l_src_fir, l_dst_fir, l_mask);
+    getColorMask(r_src_fir, r_dst_fir, r_mask);
+    proc(l_mask, l_ROI, N);
+    proc(r_mask, r_ROI, N);
+    getCenter(l_centerImg_fir, l_ROI, N);
+    getCenter(r_centerImg_fir, r_ROI, N);
+
+    getColorMask(l_src_sec, l_dst_sec, l_mask);
+    getColorMask(r_src_sec, r_dst_sec, r_mask);
+    proc(l_mask, l_ROI, N);
+    proc(r_mask, r_ROI, N);
+    getCenter(l_centerImg_sec, l_ROI, N);
+    getCenter(r_centerImg_sec, r_ROI, N);
+
+    wldDist = calcBinocularDist(l_centerImg_fir, r_centerImg_fir, l_centerImg_sec, r_centerImg_sec, f, T);
+    cout << "Distance (in mm) : " << wldDist << endl;
+    double err = (wldDist - standard) / standard;
+    cout << "Bias (0 ~ 1) : " << err << endl;
+}
 
 void proc(Mat& binary, Rect* rect, int size) {
     vector<vector<Point>> contours;
@@ -50,52 +92,22 @@ void getCenter(Point2f& center, Rect* rect, int size) {
     center.y /= size;
 }
 
-int main() {
-    Point2f l_centerImg_fir, l_centerImg_sec;
-    Point2f r_centerImg_fir, r_centerImg_sec;
-    double wldDist = 0;     // 公制位移
-    Mat l_src_fir, l_dst_fir;
-    Mat l_src_sec, l_dst_sec;
-    Mat r_src_fir, r_dst_fir;
-    Mat r_src_sec, r_dst_sec;
-    Mat l_mask, r_mask;
-    double T = 50, f = FOC_IN_PIX;
-    double standard = 55.0;
-
-    l_src_fir = imread("D:/【重要】毕业设计！！！/WPS图片批量处理/l1.png");
-    l_src_sec = imread("D:/【重要】毕业设计！！！/WPS图片批量处理/l2.png");
-    r_src_fir = imread("D:/【重要】毕业设计！！！/WPS图片批量处理/r1.png");
-    r_src_sec = imread("D:/【重要】毕业设计！！！/WPS图片批量处理/r2.png");
-
-    getColorMask(l_src_fir, l_dst_fir, l_mask);
-    getColorMask(r_src_fir, r_dst_fir, r_mask);
-    proc(l_mask, l_ROI, N);
-    proc(r_mask, r_ROI, N);
-    getCenter(l_centerImg_fir, l_ROI, N);
-    getCenter(r_centerImg_fir, r_ROI, N);
-
-    getColorMask(l_src_sec, l_dst_sec, l_mask);
-    getColorMask(r_src_sec, r_dst_sec, r_mask);
-    proc(l_mask, l_ROI, N);
-    proc(r_mask, r_ROI, N);
-    getCenter(l_centerImg_sec, l_ROI, N);
-    getCenter(r_centerImg_sec, r_ROI, N);
-
+double calcBinocularDist(Point2f left_pos1, Point2f right_pos1, Point2f left_pos2, Point2f right_pos2, double f, double T) {
     double deltaX, deltaY, deltaZ;
     double lx1, rx1, ly1, ry1, lx2, rx2, ly2, ry2, x1, x2, y1, y2, z1, z2;
-    lx1 = l_centerImg_fir.x;
-    rx1 = r_centerImg_fir.x;
-    ly1 = l_centerImg_fir.y;
-    ry1 = r_centerImg_fir.y;
+    lx1 = left_pos1.x;
+    rx1 = right_pos1.x;
+    ly1 = left_pos1.y;
+    ry1 = right_pos1.y;
     z1 = f * T / (lx1 - rx1);
     x1 = lx1 * z1 / f;
     y1 = ly1 * z1 / f;
     Mat fir_pt = (Mat_<double>(3, 1) << x1, y1, z1);
     cout << "First Point (in mm) :\n" << fir_pt << endl << endl;
-    lx2 = l_centerImg_sec.x;
-    rx2 = r_centerImg_sec.x;
-    ly2 = l_centerImg_sec.y;
-    ry2 = r_centerImg_sec.y;
+    lx2 = left_pos2.x;
+    rx2 = right_pos2.x;
+    ly2 = left_pos2.y;
+    ry2 = right_pos2.y;
     z2 = f * T / (lx2 - rx2);
     x2 = lx2 * z2 / f;
     y2 = ly2 * z2 / f;
@@ -104,8 +116,5 @@ int main() {
     deltaZ = z2 - z1;
     deltaX = x2 - x1;
     deltaY = y2 - y1;
-    wldDist = sqrt(pow(deltaZ, 2) + pow(deltaX, 2) + pow(deltaY, 2));
-    cout << "Distance (in mm) : " << wldDist << endl;
-    double err = (wldDist - standard) / standard;
-    cout << "Bias (0 ~ 1) : " << err << endl;
+    return sqrt(pow(deltaZ, 2) + pow(deltaX, 2) + pow(deltaY, 2));
 }
